fbac6e0873e99f5bfaa8b63633e15a1e45f9dd52
MinexAutomation <minex.automation@gmail.com>
2019-11-16 12:43:19 -0800

    Added geography type for identifying a set of locations.

---
+++ source/R5T.Corcyra/Code/Classes/Geography.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Corcyra
+{
+    public class Geography
+    {
+        public GeographyIdentity Identity { get; set; }
+        public List<LngLat> Vertices { get; } = new List<LngLat>();
+    }
+}
