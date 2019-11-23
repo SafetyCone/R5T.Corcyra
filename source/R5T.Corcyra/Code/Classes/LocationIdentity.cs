0e158763834d26c9a1d584788526eb0ea9dcbee5
MinexAutomation <minex.automation@gmail.com>
2019-11-22 16:07:22 -0800

    Added location identity type.

---
+++ source/R5T.Corcyra/Code/Classes/LocationIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Corcyra
+{
+    public class LocationIdentity : TypedGuid
+    {
+        #region Static
+
+        public static LocationIdentity New()
+        {
+            var locationIdentity = new LocationIdentity();
+            return locationIdentity;
+        }
+
+        public static LocationIdentity From(Guid guid)
+        {
+            var locationIdentity = new LocationIdentity(guid);
+            return locationIdentity;
+        }
+
+        #endregion
+
+
+        public LocationIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public LocationIdentity()
+            : this(TypedGuid.GetNewGuid())
+        {
+        }
+    }
+}
