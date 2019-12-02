c1fa6e3fecd5bc2cda79163a42c2139e90e6637d
MinexAutomation <minex.automation@gmail.com>
2019-12-02 00:29:41 -0800

    Added catchment name property.

--- source/R5T.Corcyra/Code/Classes/Catchment.cs
+++ source/R5T.Corcyra/Code/Classes/Catchment.cs
@@ -7,6 +7,7 @@ namespace R5T.Corcyra
     public class Catchment
     {
         public CatchmentIdentity Identity { get; set; }
+        public string Name { get; set; }
         public List<LngLat> Boundary { get; } = new List<LngLat>();
     }
 }
