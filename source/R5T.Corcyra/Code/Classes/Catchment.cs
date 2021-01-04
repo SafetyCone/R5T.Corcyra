43a3b13f0f63c847feb224b69474e7dc1edd063a
Zeph Arvanitis <tasha.arvanitis@gmail.com>
2021-01-04 09:40:40 -0600

    Add note about limitation of List<LngLat>

--- source/R5T.Corcyra/Code/Classes/Catchment.cs
+++ source/R5T.Corcyra/Code/Classes/Catchment.cs
@@ -8,6 +8,8 @@ namespace R5T.Corcyra
     {
         public CatchmentIdentity Identity { get; set; }
         public string Name { get; set; }
+        // NOTE: this approach to boundary is simplified! Anything with multiple
+        // "chunks" or holes will not be represented correctly
         public List<LngLat> Boundary { get; } = new List<LngLat>();
     }
 }
