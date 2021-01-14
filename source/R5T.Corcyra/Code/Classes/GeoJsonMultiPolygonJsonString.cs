554a3b90bca330a5ec6fb92fc619059b55ec4733
David Coats <david@getsafetycone.com>
2021-01-13 22:27:22 -0800

    Add GeoJSON schema string and GeoJSON MultiPolygon JSON string types

---
+++ source/R5T.Corcyra/Code/Classes/GeoJsonMultiPolygonJsonString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Corcyra
+{
+    public class GeoJsonMultiPolygonJsonString : TypedString
+    {
+        #region Static
+
+        public static GeoJsonMultiPolygonJsonString From(string geoJsonMultiPolygonJsonStringValue)
+        {
+            var geoJsonMultiPolygonJsonString = new GeoJsonMultiPolygonJsonString(geoJsonMultiPolygonJsonStringValue);
+            return geoJsonMultiPolygonJsonString;
+        }
+        
+        #endregion
+
+
+        public GeoJsonMultiPolygonJsonString(string value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ source/R5T.Corcyra/Code/Classes/GeoJsonSchemaString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Corcyra
+{
+    public class GeoJsonSchemaString : TypedString
+    {
+        #region Static
+
+        public static GeoJsonSchemaString From(string geoJsonSchemaStringValue)
+        {
+            var geoJsonSchemaString = new GeoJsonSchemaString(geoJsonSchemaStringValue);
+            return geoJsonSchemaString;
+        }
+
+        #endregion
+
+
+        public GeoJsonSchemaString(string value)
+            : base(value)
+        {
+        }
+    }
+}
