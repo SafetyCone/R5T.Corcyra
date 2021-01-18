642f932447d3ef6eeb2d60530be884c487ac986c
David Coats <david@getsafetycone.com>
2021-01-17 23:40:23 -0800

    Add CatchmentGeoJson type, that uses a GeoJson MultiPolygon string to transfer geometry information

---
+++ source/R5T.Corcyra/Code/Classes/CatchmentGeoJson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Corcyra
+{
+    public class CatchmentGeoJson
+    {
+        public CatchmentIdentity Identity { get; set; }
+        public string Name { get; set; }
+        public GeoJsonMultiPolygonJsonString MultiPolygonGeoJsonText { get; set; }
+    }
+}
