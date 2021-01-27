eb8a8d1e03ec3a1fa341de591e78c401ebdafe42
David Coats <david@getsafetycone.com>
2021-01-26 23:43:56 -0800

    Add BoundingBox and Location types

---
+++ source/R5T.Corcyra/Code/Classes/BoundingBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Corcyra
+{
+    public class BoundingBox
+    {
+        public double MinimumX { get; set; }
+        public double MaximumX { get; set; }
+        public double MinimumY { get; set; }
+        public double MaximumY { get; set; }
+    }
+}
---
+++ source/R5T.Corcyra/Code/Classes/Location.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Corcyra
+{
+    public class Location
+    {
+        public LocationIdentity Identity { get; set; }
+        public LngLat LngLat { get; set; }
+    }
+}
